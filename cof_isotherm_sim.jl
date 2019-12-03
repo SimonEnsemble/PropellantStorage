f6f8df3729c81963840d40fa647f7e1529a318eb
CorySimon <corysimon@berkeley.edu>
2019-12-03 12:15:16 -0800

    cory, nick meet

---
+++ cof_isotherm_sim.jl
@@ -0,0 +1,26 @@
+using PorousMaterials
+
+# read in crystal structure name from command line arguments
+if length(ARGS) != 1
+    error("pass the crystal structure name as a command line argument, such as:
+    julia cof_isotherm_sim.jl COF-102.cif")
+end
+crystal = ARGS[1]
+println("running mol sim in ", crystal)
+
+frame = Framework(crystal)
+strip_numbers_from_atom_labels!(frame)
+
+temp =  298.0 # K
+ljff = LJForceField("UFF.csv")
+
+xe = Molecule("Xe")
+
+pressures = 10 .^ range(-2, stop=log10(300), length=15) # bar
+
+n_sample_cycles = 25000
+n_burn_cycles = 25000
+
+adsorption_data = adsorption_isotherm(frame, xe, temp, pressures,
+                    ljff, n_burn_cycles=n_burn_cycles, n_sample_cycles=n_sample_cycles,
+                    eos=:PengRobinson)
--- gcmc_submit.sh
+++ gcmc_submit.sh
@@ -4,18 +4,19 @@
 # default is to use the users home directory
 #$ -cwd
 
-# name this job
-#$ -N cof_103
-
 #$ -pe thread 4 # use 4 threads/cores
 
-# send stdout and stderror to this file
-#$ -o cof_103.o
-#$ -e cof_103.e
+ # COMMENT OUT b/c we will pass these into `qsub`.
+ # # name this job
+ # #$ -N cof_103
+ # 
+ # # send stdout and stderror to this file
+ # #$ -o cof_103.o
+ # #$ -e cof_103.e
 
 # select queue - if needed; mime5 is SimonEnsemble priority queue but is restrictive.
-##$ -q mime5
+#$ -q mime5
 
 # print date and time
 date
-~/julia-1.1.0/bin/julia cof_103_isotherm_sim.jl 
+~/julia-1.3.0/bin/julia -p 4 cof_isotherm_sim.jl xtal
